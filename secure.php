dbdd7e86e73b176a9c850ab288117b2afa3b00ed
Russell Pruitt <rpruitt@ket.org>
2015-12-01 14:58:30 -0500

    don't track info

--- secure.php
+++ secure.php
@@ -2,12 +2,12 @@
 
 define("KET_ASSET", true);
 define("KET_ASSET_DBTYPE", "mysql");
-define("KET_ASSET_DBHOST", "");
-define("KET_ASSET_DBNAME", "");
-define("KET_ASSET_DBUSER", "");
-define("KET_ASSET_DBPASS", "");
-define("KET_ASSET_LDAP_HOST", "");
-define("KET_ASSET_LDAP_BASEDN", "");
+define("KET_ASSET_DBHOST", "localhost");
+define("KET_ASSET_DBNAME", "inventory");
+define("KET_ASSET_DBUSER", "db_user");
+define("KET_ASSET_DBPASS", "qwe123!@#");
+define("KET_ASSET_LDAP_HOST", "ldap://dc03.ket.local");
+define("KET_ASSET_LDAP_BASEDN", "dc=ket,dc=local");
 
 
 // closing tag left off intentionally to avoid white space
