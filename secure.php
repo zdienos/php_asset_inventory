6f7ef6ac9e6879726826baa986185fc0c0e89175
Russell Pruitt <rpruitt@ket.org>
2015-12-01 14:53:06 -0500

    add non tracked constants file for server connection info

---
+++ secure.php
@@ -0,0 +1,12 @@
+<?php
+
+define("KET_ASSET_DBTYPE", "mysql");
+define("KET_ASSET_DBHOST", "");
+define("KET_ASSET_DBUSER", "");
+define("KET_ASSET_DBPASS", "");
+
+define("KET_ASSET_LDAP_HOST", "");
+define("KET_ASSET_LDAP_BASEDN", "");
+
+
+// closing tag left off intentionally to avoid white space
