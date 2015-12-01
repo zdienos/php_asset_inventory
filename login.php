07e328ce9dccf472b98ca4a4981cb76a8c14975f
Russell Pruitt <rpruitt@ket.org>
2015-12-01 13:27:06 -0500

    structural changes as well as $USER object enhancements

--- login.php
+++ login.php
@@ -3,10 +3,6 @@
 // check for authorized post
 if( (isset($_POST['key'])) && (base64_decode($_POST['key']) !== session_id()) ) {
 
-    var_dump(session_id());
-    
-    var_dump(base64_decode($_POST['key']));
-    
     // replace this with actually error logging
     die("don't hack me foo!");
     
@@ -31,44 +27,100 @@ if( (isset($_POST['key'])) && (base64_decode($_POST['key']) !== session_id()) ) 
     
     $bind = @ldap_bind($LDAP, $ldap_rdn, $password);
 
-    $login_end = microtime(true);
-
-    
     if ($bind) {
-        
         $filter = "(sAMAccountName=$username)";
         $result = ldap_search($LDAP,$CFG->ldap_basedn,$filter);
         ldap_sort($LDAP,$result,"sn");
         $info = ldap_get_entries($LDAP, $result);
-        $fullname = $info[0]["givenname"][0]." ".$info[0]["sn"][0];
-        
+        $firstname = $info[0]["givenname"][0];
+        $lastname = $info[0]["sn"][0];
+        $fullname = $firstname." ".$lastname;
         for($i=0; $i<$info[0]["memberof"]["count"]; $i++){
             $group_dn = explode(",",$info[0]["memberof"][$i]);
             $group = explode("=",$group_dn[0]);
             $groups[] = $group[1];
         }
+        
+        $ad_dn = getDN($LDAP, $username, $CFG->ldap_basedn);
+        ldap_unbind($LDAP);
 
+    }
+
+
+    
+    // check if user has logged in before
+    $user_exists = get_user_info($username);
+    
+    
+    if(!$user_exists){
+        
+        // write user data to db if none exists (no login)
+        $creation_time = time();
+        $new_user_sql = "INSERT INTO users (username, first_name, last_name, created_at, last_update) VALUES (:user,:first,:last,:time,CURRENT_TIMESTAMP)";
+        //$new_user_sql = "INSERT INTO users (username, first_name, last_name, created_at, last_update) VALUES ('$username','$firstname','$lastname','$creation_time',CURRENT_TIMESTAMP)";
+        
+        $stmt = $SITE->DB->prepare($new_user_sql);
+        
+        $stmt->bindValue(':user', $username, PDO::PARAM_STR);
+        $stmt->bindValue(':first', $firstname, PDO::PARAM_STR);
+        $stmt->bindValue(':last', $lastname, PDO::PARAM_STR);
+        $stmt->bindValue(':time', $creation_time, PDO::PARAM_INT);
+        
+        try {
+            $stmt->execute();
+        } catch (PDOException $e) {
+            die(var_dump($e));
+            $SITE->error->add($e);
+        }
+
+        // get user id
+        $USER->id = $SITE->DB->lastInsertId();
         
         // write to USER
         $USER->logged = true;
+        $USER->firstname = $firstname;
+        $USER->lastname = $lastname;
         $USER->fullname = $fullname;
-        $USER->dn = getDN($LDAP, $username, $CFG->ldap_basedn);
+        $USER->dn = $ad_dn;
         $USER->acct = $username;
         $USER->groups = $groups;
-                
-        // write to session
-        $_SESSION['uzr']['logged'] = $USER;
-        
-        ldap_unbind($LDAP);
-        header("Location: index.php");
-        exit();
-   
+
+        // log the login
+        log_event($USER->id,'','login');
+    
     } else {
+
+        //die("<pre>".var_dump($user_exists)."</pre>");
+        // get stored user info
         
-        echo "<p>Invalid username or password</p>\n";
+        $USER->logged = true;
         
+        $USER->id = $user_exists[0]["id"];
+        $USER->username = $user_exists[0]["username"];
+        $USER->firstname = $user_exists[0]["first_name"];
+        $USER->lastname = $user_exists[0]["last_name"];
+        $USER->fullname = $USER->firstname." ".$USER->lastname;
+        $USER->dn = $ad_dn;
+        $USER->acct = $username;
+        $USER->groups = $groups;
+        
+        
+
+        //die("<p>user existed:</p><pre>".debug_dump($USER)."</pre>");
+        
+        //die(__FILE__." ".__LINE__."<br/><pre>".var_dump($USER)."</pre>");
+        // log the login
+        log_event($USER->id,'','login');
     }
 
+    // write to session
+    $_SESSION['uzr']['logged'] = $USER;
+
+    header("Location: index.php");
+    exit();
+   
+} else {
+    //echo "<p>Invalid username or password</p>\n";
 }
 
 include('login_form.php');
