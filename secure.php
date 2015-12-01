952c4bfc0a73f70aa3f1fdbea036afd9a4d8481f
Russell Pruitt <rpruitt@ket.org>
2015-12-01 14:56:40 -0500

    cleanup

--- secure.php
+++ secure.php
@@ -1,11 +1,11 @@
 <?php
 
+define("KET_ASSET", true);
 define("KET_ASSET_DBTYPE", "mysql");
 define("KET_ASSET_DBHOST", "");
 define("KET_ASSET_DBNAME", "");
 define("KET_ASSET_DBUSER", "");
 define("KET_ASSET_DBPASS", "");
-
 define("KET_ASSET_LDAP_HOST", "");
 define("KET_ASSET_LDAP_BASEDN", "");
 
