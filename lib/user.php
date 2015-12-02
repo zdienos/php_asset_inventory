55b27b704cdb46c51b5d6d18f9afb9b1dca514f2
Russell Pruitt <rpruitt@ket.org>
2015-12-02 09:50:11 -0500

    renaming of constants

--- lib/user.php
+++ lib/user.php
@@ -4,7 +4,7 @@
  * George Russell Pruitt <pruitt.russell@gmail.com>
  */
 
-defined("BAREBONES_CORE") || die("External linking to the file is restricted");
+defined("KET_ASSET") || die("External linking to the file is restricted");
 
 // set the $SITE global object
 unset($USER);
