38496ef47a5bd91bbf773dbacec6bee8522c4847
Russell Pruitt <rpruitt@ket.org>
2015-12-04 16:33:05 -0500

    Fixed bugs in menu display

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,40 @@
 $(document).ready(function(){
+
+    
+    // show / hide menu
     $( '#nav-toggle-button' ).on( "click", function( event ){
+        
+        console.log(this);
+        console.log("button clicked");
+        
+        // grab items
+        var menuDiv = $( '#nav-menu-links' );
+        console.log(menuDiv);
+        var menuButton = $( this );
+        console.log(menuButton);
+        
+        // grab mouse pos
         var mouseX = event.pageX;
         var mouseY = event.pageY;
-        var menuButton = $( '#nav-button-toggle' );
-        var buttonPos = menuButton.position();
-        var buttonWidth = menuButton.width();
-        var buttonHeight = menuButton.height();
-        var menuTop = buttonPos.top + button
-        $( '#nav-menu-links' ).toggle();
+    
+        // get dimensions of menu
+        var menuWidth = menuDiv.width();
+        var menuHeight = menuDiv.height();
+        var menuTop = $( '#content-wrap' ).position().top;
+        //var menuLeft = $( '#content-wrap' ).width() - menuWidth;
+        var fullWidth = $( '.container' ).outerWidth(true)
+        var offsetX = ( fullWidth - $( '.container' ).width() ) / 2;
+        var menuLeft = ( $( '.container' ).width() + offsetX ) - menuWidth;
+
+        console.log(menuTop);
+        console.log(menuLeft);
+        menuDiv.css({top: menuTop, left: menuLeft, position:'absolute'});
+        console.log(menuDiv);
+        $( menuDiv ).toggle();
         
     });
+    
+    
+    $( document ).ready(function(){});
+    
 });
