1cc1c58f50d56db5751968b32d7a8f3cab1e3b6a
Russell Pruitt <rpruitt@ket.org>
2015-12-09 14:10:07 -0500

    new files

---
+++ edit.php
@@ -0,0 +1,20 @@
+<?php
+
+// config
+require_once("config.php");
+
+if($CFG->debug){
+    echo debug_dump($SITE, get_var_name($SITE));
+}
+
+require_once('header.php');
+
+// check for login
+if( !$USER->logged ){
+    header('login.php');
+}
+
+
+
+require_once('footer.php');
+?>
