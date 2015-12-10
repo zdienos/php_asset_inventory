3751d21dad81d52b591b967950b986991ea892b6
Russell Pruitt <pruitt.russell@gmail.com>
2015-12-10 11:04:23 -0500

    beginning stages of LDAP authorization

--- left-nav.php
+++ left-nav.php
@@ -4,16 +4,9 @@ $output = "<ul>".PHP_EOL;
 
 if($USER->logged){ 
 
-    $is_admin = FALSE;
-    
-    // check for admin group
-    if(in_array( "KET Administrators", $USER->session->groups )){
-        $is_admin = TRUE;
-    }
-    
     $output .= "\t<li><a href='index.php'>Home</a></li>".PHP_EOL;
     
-    if($is_admin){
+    if($USER->is_admin){
         $output .= "\t<li><a href='edit.php?id=new'>New Asset</a></li>".PHP_EOL;
         $output .= "\t<li><a href='browse.php'>Browse</a></li>".PHP_EOL;
     }
