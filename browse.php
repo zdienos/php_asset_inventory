1ccb9503e88566906dbf19e9cdc9018927a11b32
Russell Pruitt <pruitt.russell@gmail.com>
2016-01-04 11:29:54 -0500

    sql for search began

--- browse.php
+++ browse.php
@@ -14,7 +14,7 @@ if( !$USER->logged ){
     header('login.php');
 }
 
-// 
+// check for post and validate
 if(!empty($_POST)){
     
     // validate POST
@@ -30,14 +30,47 @@ if(!empty($_POST)){
         die("don't hack me bro");
     }
     
+    /*
     foreach($_POST as $key => $value){
         echo "<p>$key: <strong>[$value]</strong></p>".PHP_EOL;
     }
+    */
     
 } else {
     die("page missing required components");
 }
 
-require_once('footer.php');
+// begin building SQL
+$search_sql = "SELECT ";
+$search_sql .= " * ";
+$search_sql .= "FROM assets ";
+$search_sql .= "INNER JOIN asset_type ON assets.type_id = asset_type.id ";
+$search_sql .= "INNER JOIN asset_status ON assets.status_id = asset_status.id ";
 
+// build where clause
+$search_sql .= "WHERE ";
+$where_values = array();
+
+// build where array
+foreach($_POST as $key => $value){
+    if( ($key !== "valid") && ( !empty($value) ) ){
+        $where_values[] .= " $key = '$value' ";
+    }
+}
+
+foreach($where_values as $clause){
+    if($clause === end($where_values)){
+        $search_sql .= $clause;
+    } else {
+        $search_sql .= $clause." AND ";
+    }
+}
+
+// TODO order sql
+$search_sql .= "";
+
+// TODO limit(pagination) sql
+$search_sql .= "";
+
+require_once('footer.php');
 ?>
