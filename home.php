15c52355e8a85b40806e857a78b8102c23252818
Russell Pruitt <pruitt.russell@gmail.com>
2016-01-28 14:12:55 -0500

    ajax dropdowns

--- home.php
+++ home.php
@@ -1,38 +1,73 @@
 <?php
 
-// TODO grab actual values from table
-// grab models
-$types = array();
-$types[] = array("1","Computer");
-$types[] = array("2","Tablet");
-$types[] = array("3","Other");
 
-// build models dropdown output
-$types_dropdown_output = "<select id='types-dropdown' name='type_id'>".PHP_EOL;
-$types_dropdown_output .= "<option>Select Below</option>".PHP_EOL;
+// build type options
+$types = get_asset_types();
+$types_dropdown_output = "<select id='type_id' name='type_id'>".PHP_EOL;
+$types_dropdown_output .= "<option>Select Below</option>";
 foreach($types as $type){
-    $types_dropdown_output .= "<option value='$type[0]'>$type[1]</option>".PHP_EOL;
+    $types_dropdown_output .= "<option value='".$type['id']."'";
+    if( (isset($asset)) && ($type['id'] === $asset['type_id']) ){
+        $types_dropdown_output .= " selected";
+    }
+    $types_dropdown_output .= ">".$type['type']."</option>".PHP_EOL;
 }
 $types_dropdown_output .= "</select>".PHP_EOL;
 
-// TODO grab actual values from table
-// grab models
-$models = array();
-$models[] = array("1","Optiplex 7020");
-$models[] = array("2","Optiplex 7010");
-$models[] = array("3","Optiplex 780");
 
-// build models dropdown output
-$models_dropdown_output = "<select id='models-dropdown' name='model'>".PHP_EOL;
-$models_dropdown_output .= "<option>Select Below</option>".PHP_EOL;
-foreach($models as $model){
-    $models_dropdown_output .= "<option value='$model[0]'>$model[1]</option>".PHP_EOL;
+// build status options
+$statuses = get_asset_statuses();
+$statuses_dropdown_out = "<select id='status_id' name='status_id'>".PHP_EOL;
+$statuses_dropdown_out .= "<option>Select Below</option>";
+foreach($statuses as $status){
+    $statuses_dropdown_out .= "<option value='".$status['id']."'";
+    if( (isset($asset)) && ($status['id'] === $asset['status_id']) ){
+        $statuses_dropdown_out .= " selected";
+    }
+    $statuses_dropdown_out .= ">".$status['status']."</option>".PHP_EOL;
 }
+$statuses_dropdown_out .= "</select>".PHP_EOL;
+
+
+// build make options
+$makes = get_asset_makes();
+$makes_dropdown_out = "<select id='make_id' name='make_id'>".PHP_EOL;
+$makes_dropdown_out .= "<option>Select Below</option>";
+foreach($makes as $make){
+    $makes_dropdown_out .= "<option value='".$make['id']."'";
+    if( (isset($asset)) && ($make['id'] === $asset['make_id']) ){
+        $makes_dropdown_out .= " selected";
+    }
+    $makes_dropdown_out .= ">".$make['make']."</option>".PHP_EOL;
+}
+$makes_dropdown_out .= "</select>".PHP_EOL;
+
+
+// build model options
+$models = get_asset_models();
+$models_dropdown_output = "<select id='model_id' name='model_id'>".PHP_EOL;
+//$models_dropdown_output .= "<option>Pick Make First</option>";
+/*
+$models_dropdown_output .= "<option>Select Below</option>";
+foreach($models as $model){
+    $models_dropdown_output .= "<option value='".$model['id']."'";
+    if( (isset($asset)) && ($model['id'] === $asset['model_id']) ){
+        $models_dropdown_output .= " selected";
+    }
+    $models_dropdown_output .= ">".$model['model']."</option>".PHP_EOL;
+}
+*/
 $models_dropdown_output .= "</select>".PHP_EOL;
-?>
-<div>
+
+$last_login_epoch = get_user_last_login($USER->session->id);
+$last_login = date('Y-m-d H:i:s',$last_login_epoch);
+
+$new_devices = get_new_assets($USER->session->id,true);
+
+?><div>
     
     <p>Welcome <strong><?php echo $USER->session->fullname; ?></strong></p>
+    <p>There are <strong><?php echo $new_devices ?></strong> new devices since your last login at: <strong><?php echo $last_login; ?></strong></p>
     
     <div id="content-search-box">
         <form action="browse.php" method="post">
@@ -43,20 +78,27 @@ $models_dropdown_output .= "</select>".PHP_EOL;
                 <label>Type</label>
                 <?php echo $types_dropdown_output; ?>
             </div>
+            <div id="make-search">
+                <label>Make</label>
+                <?php echo $makes_dropdown_out; ?>
+            </div>
             <div id="model-search">
                 <label>Model</label>
                 <?php echo $models_dropdown_output; ?>
+            </div>
+           <div id="status-search">
+                <label>Status</label>
+                <?php echo $statuses_dropdown_out; ?>
             </div>
             <div id="asset-search">
                 <label>Asset Tag</label>
                 <input type="text" id="asset_tag" name="asset_tag" />
             </div>
+
         <input type="submit" value="Go">
         </form>
     </div>
     
-    <div></div>
-    
-    <div></div>
+	<script src="<?php echo $SITE->CFG->js; ?>home.js"></script>
     
 </div>
---
+++ js/home.js
@@ -0,0 +1,57 @@
+/*
+function grabModels(dataObj){
+    
+	var nxtElem = $("#model_id");
+
+	$.ajax({
+        url:"admin/get_makes.php",
+        type:"post",
+        data:dataObj,
+        dataType:"json", // <-------------expecting json from php
+        success:function(data){
+           $(nxtElem).empty(); // empty the field first here.
+           $.each(data, function(i, obj){
+               $(nxtElem).html(obj);
+           });
+        },
+        error:function(err){
+           console.log(err);
+        }
+    });
+}
+*/
+
+function grabModels(dataObj){
+    
+	var nxtElem = $("#model_id");
+
+	$.ajax({
+		url:"admin/get_makes.php",
+		type:"post",
+		data:dataObj,
+		dataType:"json", // <-------------expecting json from php
+		success:function(data){
+			$(nxtElem).empty(); // empty the field first here.
+			$.each(data, function(i, obj){
+				console.log(obj);
+				$('<option>',{
+					value:obj.id,
+					text:obj.model
+				}).appendTo(nxtElem);		 
+			});
+        },
+        error:function(err){
+           console.log(err);
+        }
+    });
+}
+
+
+$(function(){
+    $('#make_id').on('change', function(e){
+        if(this.id === "make_id"){
+          var dataObj = {make_id:this.value};
+          grabModels(dataObj);
+        }
+    });
+});
