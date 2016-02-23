c984acba7f7ca3747f845d4cee0dc044657a74ec
Russell Pruitt <pruitt.russell@gmail.com>
2016-02-23 11:54:15 -0500

    admin landing page

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once("../config.php");
+
+?><?php include_once("../header.php");?>
+
+<div>
+	<ul>
+		<li><a href="makes.php">Makes</a></li>
+		<li><a href="models.php">Models</a></li>
+		<li><a href="types.php">Types</a></li>
+		<li><a href="statuses.php">Statuses</a></li>
+	</ul>
+</div>
+
+<?php include_once("../footer.php");?>
