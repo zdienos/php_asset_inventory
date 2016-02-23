20fff63d86ad220b919733afb9d90e9a8d293474
Russell Pruitt <pruitt.russell@gmail.com>
2016-02-23 11:55:57 -0500

    menu tweaks and updates

--- nav-menu.php
+++ nav-menu.php
@@ -10,22 +10,24 @@ if($USER->logged){
         $menu_output .= "\t\t\t<!-- logged in -->".PHP_EOL;
     }
     $menu_output .= "\t\t\t<li><a href='".$SITE->CFG->url."index.php'>Home</a></li>".PHP_EOL;
-    
     if($USER->is_admin){
+		$menu_output .= "\t\t\t<li><a href='".$SITE->CFG->url."reports/activity.php'>Activity Log</a></li>".PHP_EOL;
         $menu_output .= "\t\t\t<li><a href='".$SITE->CFG->url."admin/edit.php'>New Asset</a></li>".PHP_EOL;
         $menu_output .= "\t\t\t<li><a href='".$SITE->CFG->url."browse.php'>Browse</a></li>".PHP_EOL;
+		$menu_output .= "\t\t\t<li><a href='".$SITE->CFG->url."admin/index.php'>Admin</a></li>".PHP_EOL;
     }
-    
     $menu_output .= "\t\t\t<li><a href='".$SITE->CFG->url."reports.php'>Reports</a></li>".PHP_EOL;
-    $menu_output .= "\t\t\t<li role='separator' class='divider'></li>".PHP_EOL;
-
+	
 } else {
-    if($SITE->CFG->debug){
+
+	if($SITE->CFG->debug){
         $menu_output .= "\t\t\t<!-- not logged in -->".PHP_EOL;
     }
     $menu_output .= "\t\t\t<li><a href='".$SITE->CFG->url."index.php'>Home</a></li>".PHP_EOL;
 }
 
+$menu_output .= "\t\t\t<li role='separator' class='divider'></li>".PHP_EOL;
+$menu_output .= "\t\t\t<li><a href='".$SITE->CFG->url."logout.php'>Logout</a></li>".PHP_EOL;
 $menu_output .= "\t\t</ul>".PHP_EOL;
 
 //$menu_output .= "</nav>".PHP_EOL;
