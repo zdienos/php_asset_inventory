cb5290ca03a2e703378b45337df9aacf7df9ddfe
Russell Pruitt <pruitt.russell@gmail.com>
2016-02-23 11:55:48 -0500

    reports landing page

--- reports.php
+++ reports.php
@@ -14,6 +14,36 @@ if( !$USER->logged ){
     header('login.php');
 }
 
+//$reports_output .= "";
+
+$reports_output = "<div id='report_menu'>";
+
+// generate admin report section
+if($USER->is_admin){
+	$reports_output .= "<fieldset>";
+	$reports_output .= "<legend>Admin Reports</legend>";
+	$reports_output .= "<p>Insert report link here</p>";
+	$reports_output .= "<p>Insert report link here</p>";
+	$reports_output .= "<p>Insert report link here</p>";
+	$reports_output .= "</fieldset>";
+}
+
+// generate user reports
+$reports_output .= "<fieldset>";
+$reports_output .= "<legend>User Reports</legend>";
+$reports_output .= "<p>Insert report link here</p>";
+$reports_output .= "<p>Insert report link here</p>";
+$reports_output .= "<p>Insert report link here</p>";
+$reports_output .= "<p>Insert report link here</p>";
+$reports_output .= "<p>Insert report link here</p>";
+$reports_output .= "</fieldset>";
+
+
+$reports_output .= "</div>";
+
+
+echo $reports_output;
+
 require_once('footer.php');
 
 ?>
