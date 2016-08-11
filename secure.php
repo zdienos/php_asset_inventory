d2403c46b85240d2df69b21efc271e6994e32e47
Russell Pruitt <pruitt.russell@gmail.com>
2016-08-11 12:06:15 -0400

    update groups

--- secure.php
+++ secure.php
@@ -2,16 +2,16 @@
 define("KET_ASSET", true);
 define("KET_ASSET_DBTYPE", "mysql");
 define("KET_ASSET_DBHOST", "localhost");
-define("KET_ASSET_DBNAME", "inventory");
-define("KET_ASSET_DBUSER", "db_user");
-define("KET_ASSET_DBPASS", "qwe123!@#");
+define("KET_ASSET_DBNAME", "");
+define("KET_ASSET_DBUSER", "");
+define("KET_ASSET_DBPASS", "");
 /*
-define("KET_ASSET_DBNAME", "asset_inventory");
-define("KET_ASSET_DBUSER", "inventory");
-define("KET_ASSET_DBPASS", "d3v1ce$");
+define("KET_ASSET_DBNAME", "");
+define("KET_ASSET_DBUSER", "");
+define("KET_ASSET_DBPASS", "");
 */
-define("KET_ASSET_LDAP_HOST", "ldap://dc03.ket.local");
+define("KET_ASSET_LDAP_HOST", "ldap://dc04.ket.local");
 define("KET_ASSET_LDAP_BASEDN", "dc=ket,dc=local");
-define("KET_ASSET_GROUPS_ADMIN", "KET Administrators");
-define("KET_ASSET_GROUPS_EDIT", "Asset Inventory Edit");
+define("KET_ASSET_GROUPS_ADMIN", "IT-inventory-manage");
+define("KET_ASSET_GROUPS_EDIT", "IT-inventory-view");
 // closing tag left off intentionally to avoid white space
