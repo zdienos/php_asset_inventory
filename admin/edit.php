d3f0175beb430029c65e1104526a23ed16463bd5
Russell Pruitt <pruitt.russell@gmail.com>
2016-09-30 15:38:25 -0400

    temporarily include assign-old into edit

--- admin/edit.php
+++ admin/edit.php
@@ -327,7 +327,7 @@ if($SITE->error->has_errors()){
 		</div>
     </form>
 
-<?php require_once('assign.php'); ?>
+<?php require_once('assign-old.php'); ?>
 
 <script src="<?php echo $SITE->CFG->js; ?>jquery.validate.min.js"></script>
 <script src="<?php echo $SITE->CFG->js; ?>jquery.metadata.js"></script>
