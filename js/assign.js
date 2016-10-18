6ca529412e42cc145b0c7e4ca531ca14d7a14a46
Russell Pruitt <pruitt.russell@gmail.com>
2016-10-18 17:00:48 -0400

    more work on assign

--- js/assign.js
+++ js/assign.js
@@ -5,7 +5,29 @@ this file containts the code to populate appropriate values into the assigned_to
 */
 
 jQuery( document ).ready(function() {
-	$( "#assignment_type" ).change(function(){
-		$( "#assigned_to").append("<option value='1'>Test</option>");
+	
+	var typeSelect, assignedSelect
+	typeSelect = $( "#assignment_type" );
+	assignedSelect = $( "#assigned_to")''
+
+	typeSelect.change(function(){
+		
+		// get value of type
+		
+		// submit value of type
+		
+		// grab response
+		
+		// process response
+		
+		/*
+		$.each(selectValues, function(key, value) {   
+     	$('#mySelect')
+			.append($('<option>', { value : key })
+			.text(value)); 
+		});
+		*/
+		
+		assignedSelect.append("<option value='1'>Test</option>");
 	});
 });	
