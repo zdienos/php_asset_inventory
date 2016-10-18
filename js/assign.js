caf4fdded670a01eff72d8af4774fdbeb5736412
Russell Pruitt <pruitt.russell@gmail.com>
2016-10-18 12:43:51 -0400

    add tracking for javascript to populate the assigned_to field

---
+++ js/assign.js
@@ -0,0 +1,11 @@
+/*
+
+this file containts the code to populate appropriate values into the assigned_to field
+
+*/
+
+jQuery( document ).ready(function() {
+	$( "#make_select" ).change(function(){
+		
+	});
+});	
