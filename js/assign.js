3c9c8d4420c5279e015cf1ac89eae9827d5bdba8
Russell Pruitt <pruitt.russell@gmail.com>
2016-10-19 11:18:40 -0400

    added date picker for date fields, more code for ajax response

--- js/assign.js
+++ js/assign.js
@@ -6,6 +6,35 @@ this file containts the code to populate appropriate values into the assigned_to
 
 jQuery( document ).ready(function() {
 	
+	// datepicker for assignment start
+    $( "#assignment_start" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: "yy-mm-dd",
+        minDate: null,
+        maxDate:  null,
+        onSelect: function(selected) {
+            $( "#assignment_start" ).datepicker("option","minDate",selected)
+        }
+    });
+
+	
+	// datepicker for assignment end
+    $( "#assignment_end" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: "yy-mm-dd",
+        minDate: null,
+        maxDate:  null,
+        onSelect: function(selected) {
+            $( "#assignment_end" ).datepicker("option","minDate",selected)
+        }
+    });
+
+});
+
+jQuery( document ).ready(function() {
+
 	var typeSelect, assignedSelect
 	typeSelect = $( "#assignment_type" );
 	assignedSelect = $( "#assigned_to");
@@ -13,8 +42,15 @@ jQuery( document ).ready(function() {
 	typeSelect.change(function(){
 		
 		// get value of type
+		var dataId = typeSelect.val;
 		
 		// submit value of type
+		$.get( "get_assigned_to.php", { id: dataId } ).done( function ( resp ){
+			$.each( resp, function ( key, value ){
+				console.log( key + ": " + value );
+			});
+		});
+	
 		
 		// grab response
 		var tempVal = [1,"Russell Pruitt"];
@@ -24,9 +60,9 @@ jQuery( document ).ready(function() {
 		
 		// process response
 		$.each(tempValues, function(key, value) {   
-     	$(assignedSelect)
-			.append($('<option>', { value : key })
-			.text(value)); 
+			$(assignedSelect)
+				.append($('<option>', { value : key })
+				.text(value)); 
 		});
 		
 		
