5593ab3f4b727db04488c2cc77586b271c5e056b
Russell Pruitt <pruitt.russell@gmail.com>
2016-10-19 13:13:57 -0400

    remove old form

--- admin/edit.php
+++ admin/edit.php
@@ -329,8 +329,6 @@ if($SITE->error->has_errors()){
 
 
 <?php require_once('assign.php'); ?>
-<?php require_once('assign-old.php'); ?>
-
 
 <script src="<?php echo $SITE->CFG->js; ?>jquery.validate.min.js"></script>
 <script src="<?php echo $SITE->CFG->js; ?>jquery.metadata.js"></script>
