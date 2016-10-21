6f6519c252496da989e8534de29a1ae1d13e6449
Russell Pruitt <pruitt.russell@gmail.com>
2016-10-21 10:22:12 -0400

    added style to container

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 
 </head>
 <body>
-	<div class="container">
+	<div class="container" class="container-fluid">
         <div id="header">
             <h3>KET Asset Inventory</h3>
             <button id="nav-toggle-button" type="button" class="btn btn-default" aria-label="Left Align">
