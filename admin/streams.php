520f36802597ae3ca14102fbd037fbbc902c50a0
Russell Pruitt <pruitt.russell@gmail.com>
2016-11-01 10:13:01 -0400

    add new diagnostic tool

---
+++ admin/streams.php
@@ -0,0 +1,15 @@
+<html>
+	<head></head>
+	<body>
+		<h3>Main</h3>
+		<iframe src="http://content.streamhoster.com/player/lv_ket_webf1/broadcast1?autoPlay=1&amp;showPlayPauseBtn=0" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<h3>Partner 1</h3>
+		<iframe src="http://web28.streamhoster.com/ket_web/player.html?filePath=partner1&serviceName=lv_ket_webf1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<h3>Partner 2</h3>
+		<iframe src="http://web28.streamhoster.com/ket_web/player.html?filePath=partner2&serviceName=lv_ket_webf1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<h3>Partner 3</h3>
+		<iframe src="http://web28.streamhoster.com/ket_web/player.html?filePath=partner3&serviceName=lv_ket_webf1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<h3>Partner 4</h3>
+		<iframe src="http://web28.streamhoster.com/ket_web/player.html?filePath=partner4&serviceName=lv_ket_webf1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+	</body>
+</html>
