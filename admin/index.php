8d9db4a6667afef6b7d525324576f6a0e6b8bbf3
Russell Pruitt <pruitt.russell@gmail.com>
2016-11-01 14:10:06 -0400

    update admin page links

--- admin/index.php
+++ admin/index.php
@@ -10,6 +10,13 @@ require_once("../config.php");
 		<li><a href="models.php">Models</a></li>
 		<li><a href="types.php">Types</a></li>
 		<li><a href="statuses.php">Statuses</a></li>
+		<li><a href="departments.php">Departments</a></li>
+		<li><a href="projects.php">Projects</a></li>
+		<li><a href="rooms.php">Rooms</a></li>
+	</ul>
+	<h3>Others</h3>
+	<ul>
+		<li><a href="streams.php">StreamHoster</a></li>
 	</ul>
 </div>
 
