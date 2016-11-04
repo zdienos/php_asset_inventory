4d507086d4e706ca8e7db1d2f46e04de373f0621
Russell Pruitt <pruitt.russell@gmail.com>
2016-11-04 11:13:46 -0400

    add additional captions stream

--- admin/streams.php
+++ admin/streams.php
@@ -3,13 +3,17 @@
 	<body>
 		<h3>Main</h3>
 		<iframe src="http://content.streamhoster.com/player/lv_ket_webf1/broadcast1?autoPlay=1&amp;showPlayPauseBtn=0" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		
+		<h3>With Captions</h3>
+		<iframe class="resp-vid" style="width: 100%; position: absolute; top: 0; left: 0; height: 100%;" src="http://content.streamhoster.com/player/lv_ket_webf1/broadcast1?autoPlay=1&showPlayPauseBtn=0&captions=1" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+		
 		<h3>Partner 1</h3>
-		<iframe src="http://web28.streamhoster.com/ket_web/player.html?filePath=partner1&serviceName=lv_ket_webf1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<iframe src="http://content.streamhoster.com/preview/lv_ket_webf1/partner1?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
 		<h3>Partner 2</h3>
-		<iframe src="http://web28.streamhoster.com/ket_web/player.html?filePath=partner2&serviceName=lv_ket_webf1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<iframe src="http://content.streamhoster.com/preview/lv_ket_webf1/partner2?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
 		<h3>Partner 3</h3>
-		<iframe src="http://web28.streamhoster.com/ket_web/player.html?filePath=partner3&serviceName=lv_ket_webf1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<iframe src="http://content.streamhoster.com/preview/lv_ket_webf1/partner3?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
 		<h3>Partner 4</h3>
-		<iframe src="http://web28.streamhoster.com/ket_web/player.html?filePath=partner4&serviceName=lv_ket_webf1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<iframe src="http://content.streamhoster.com/preview/lv_ket_webf1/partner4?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
 	</body>
 </html>
