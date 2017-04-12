9647eb2f1be07029de4882b97ae8d6a578faf336
Russell Pruitt <pruitt.russell@gmail.com>
2017-04-12 11:15:57 -0400

    add csv option for existing activity report

--- reports/activity.php
+++ reports/activity.php
@@ -12,35 +12,58 @@ if( !$USER->is_admin ){
 	header('Location: '.$SITE->CFG->url.'index.php');
 }
 
+if( isset($_GET['csv']) && !empty($_GET['csv'])){
+	$csv_report = false;
+} else {
+	$csv_report = true;
+}
+
 // begin SQL
 $sql = "SELECT ";
 
 // FIELDS TO GRAB
 $sql .= "assets.id as 'asset_id', ";
+$sql .= "makes.make as 'Make', ";
+$sql .= "models.model as 'Model', ";
+$sql .= "assets.asset_tag as 'KET Tag', ";
+$sql .= "assets.serial_number as 'SN', ";
 $sql .= "CONCAT(users.first_name,' ',users.last_name) as 'fullname', ";
-$sql .= "assets.asset_tag as 'KET Tag #', ";
-$sql .= "FROM_UNIXTIME(log.time_updated) as 'Action Time', ";
-$sql .= "log.action as 'Action' ";
+
 
 // FROM AND JOINS
-$sql .= "FROM log ";
-$sql .= "LEFT JOIN assets ON log.asset_id = assets.id ";
-$sql .= "INNER JOIN users ON log.user_id = users.id ";
+$sql .= " FROM assets ";
+$sql .= " INNER JOIN asset_makes makes ON assets.make_id = asset_makes.id ";
+$sql .= " INNER JOIN asset_models models ON assets.model_id = asset_models.id ";
+$sql .= " LEFT JOIN asset_assignments assigns ON assigns.asset_id = assets.id ";
+$sql .= " LEFT JOIN users ON assigns.assigned_to = users.id ";
 
 // WHERE CLAUSE
 
+
 // ORDER CLAUSE
 
-$sql .= "ORDER BY log.time_updated DESC ";
+$sql .= "ORDER BY assets.model_id ASC ";
 
 $stmt = $SITE->DB->query($sql);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
-require_once("../header.php");
 
-//echo generate_html_table($results,"id",FALSE);
-echo generate_html_table($results,"asset_id");
 
-require_once("../footer.php");
+if($csv_report = true){
+	header("Content-type: text/csv");
+	header("Content-Disposition: attachment; filename=report.csv");
+	header("Pragma: no-cache");
+	header("Expires: 0");
+	//echo generate_csv($user_data,"id",false);
+	echo generate_csv($results,"asset_id");
+	echo "Total Records,".sizeof($results).PHP_EOL;
+	
+} else {
+	require_once("../header.php");
+	echo generate_html_table($results,"asset_id");
+	require_once("../footer.php");
+}
+
+
 ?>
