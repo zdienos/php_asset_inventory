1fa1540e2228aed5722fb9e645c054d9680807bb
Russell Pruitt <pruitt.russell@gmail.com>
2018-03-13 10:16:25 -0400

    added link to new user form

--- admin/index.php
+++ admin/index.php
@@ -13,6 +13,7 @@ require_once("../config.php");
 		<li><a href="departments.php">Departments</a></li>
 		<li><a href="projects.php">Projects</a></li>
 		<li><a href="rooms.php">Rooms</a></li>
+		<li><a href="users.php">Users</a></li>
 	</ul>
 	<h3>Others</h3>
 	<ul>
