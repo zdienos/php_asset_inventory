3fec55dfa2edfaaf2f357f78a3849b222b1cbda1
George Russell Pruitt <pruitt.russell@gmail.com>
2018-06-05 11:36:36 -0400

    Updated links for https

--- admin/streams.php
+++ admin/streams.php
@@ -2,18 +2,18 @@
 	<head></head>
 	<body>
 		<h3>Main</h3>
-		<iframe src="http://content.streamhoster.com/player/lv_ket_webf1/broadcast1?autoPlay=1&amp;showPlayPauseBtn=0" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<iframe src="https://content.streamhoster.com/player/lv_ket_webf1/broadcast1?autoPlay=1&amp;showPlayPauseBtn=0" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
 		
 		<h3>With Captions</h3>
-		<iframe src="http://content.streamhoster.com/player/lv_ket_webf1/broadcast1?autoPlay=1&showPlayPauseBtn=0&captions=1" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+		<iframe src="https://content.streamhoster.com/player/lv_ket_webf1/broadcast1?autoPlay=1&showPlayPauseBtn=0&captions=1" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
 		
 		<h3>Partner 1</h3>
-		<iframe src="http://content.streamhoster.com/preview/lv_ket_webf1/partner1?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<iframe src="https://content.streamhoster.com/preview/lv_ket_webf1/partner1?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
 		<h3>Partner 2</h3>
-		<iframe src="http://content.streamhoster.com/preview/lv_ket_webf1/partner2?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<iframe src="https://content.streamhoster.com/preview/lv_ket_webf1/partner2?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
 		<h3>Partner 3</h3>
-		<iframe src="http://content.streamhoster.com/preview/lv_ket_webf1/partner3?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<iframe src="https://content.streamhoster.com/preview/lv_ket_webf1/partner3?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
 		<h3>Partner 4</h3>
-		<iframe src="http://content.streamhoster.com/preview/lv_ket_webf1/partner4?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
+		<iframe src="https://content.streamhoster.com/preview/lv_ket_webf1/partner4?autoPlay=1&captions=1" width="640" height="360" frameborder="0" scrolling="no" webkitAllowFullScreen allowFullScreen></iframe>
 	</body>
 </html>
