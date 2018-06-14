444d9ea7a27f42a2bc59c2c076a4dd823a67abf0
George Russell Pruitt <pruitt.russell@gmail.com>
2018-06-14 11:11:38 -0400

    update html

--- nav-search.php
+++ nav-search.php
@@ -1,12 +1,10 @@
 <form action="browse.php" method="post" role="form">
     <div class="form-group">
         <input type="hidden" name="valid" value="<?php echo $USER->key; ?>" />
-        <p>
+        <span>
             <label>Asset Tag</label>
             <input type="text" id="asset_tag" name="asset_tag" value="<?php echo $asset_tag; ?>" class=".form-control" />
-        </p>
-        <p>
-            <input type="submit" value="Go">
-        </p>
-    </div>	
+            <input type="submit" value="->">
+        </span>
+    </div>
 </form>
