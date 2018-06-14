97418b349661f0ff707b24549e45697094b47fe5
George Russell Pruitt <pruitt.russell@gmail.com>
2018-06-14 11:24:58 -0400

    another html update

--- header.php
+++ header.php
@@ -30,9 +30,9 @@
 	<div class="container" class="container-fluid">
         <div id="header">
             <h3>KET Asset Inventory</h3>
-            <div id="nav-search">
-                <?php include("nav-search.php"); ?>
-            </div>
+
+            <?php include("nav-search.php"); ?>
+
             <button id="nav-toggle-button" type="button" class="btn btn-default" aria-label="Left Align">
                 <span style="font-size:16px" class="glyphicon glyphicon-menu-hamburger"></span>
             </button>
